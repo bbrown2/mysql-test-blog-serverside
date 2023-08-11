aae7ee016e04a883486a5265c46c991faed27ec9
bbrown2 <54606794+bbrown2@users.noreply.github.com>
2023-08-11 18:58:33 -0400

    update mysql connection

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "bs76ujophzwn4tv4",
-  password: "or02rhulvefagbba",
+  host: "localhost",
+  user: "root",
+  password: "password",
   port: "3306",
-  database: "htl2ei3zxo5qjpv0",
+  database: "blog_v1",
 });
